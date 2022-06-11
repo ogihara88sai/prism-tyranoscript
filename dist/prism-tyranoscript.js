ed51cb3310da92f98be60d0cefa2904597552b5b
ogihara88sai <ogihara88sai@gmail.com>
2022-06-12 07:33:33 +0900

    Initial commit

---
+++ dist/prism-tyranoscript.js
@@ -0,0 +1,160 @@
+/* prism-tyranoscript.js v1.0.0
+ * (c) 2022 @ogihara88sai
+ * Released under the MIT Licence */
+Prism.languages.tyranoscript = {
+  comment: [
+    {
+      pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+      lookbehind: true,
+      greedy: true,
+    },
+    {
+      pattern: /(^|[^\\:]);.*/,
+      lookbehind: true,
+      greedy: true,
+    },
+  ],
+  script: [
+    {
+      pattern: /(\[iscript\])[\s\S]*?(?=\[endscript\])/i,
+      lookbehind: true,
+      greedy: true,
+      inside: {
+        'language-javascript': {
+          pattern: /[\s\S]+/,
+          inside: Prism.languages.javascript,
+        },
+      },
+    },
+    {
+      pattern: /(@iscript\s*\n)[\s\S]*?(?=@endscript)/i,
+      lookbehind: true,
+      greedy: true,
+      inside: {
+        'language-javascript': {
+          pattern: /[\s\S]+/,
+          inside: Prism.languages.javascript,
+        },
+      },
+    },
+  ],
+  html: {
+    pattern: /(\[html\])[\s\S]*?(?=\[endhtml\])/i,
+    lookbehind: true,
+    greedy: true,
+    inside: {
+      'language-html': {
+        pattern: /[\s\S]+/,
+        inside: Prism.languages.html,
+      },
+    },
+  },
+  keyword: {
+    pattern: /(^|\n)\s*\*[^\n]*/,
+    greedy: true,
+    lookbehind: true,
+  },
+  'class-name': {
+    pattern: /(^|\n)\s*#[^\n]*/,
+    greedy: true,
+    /*
+    inside: {
+      punctuation: /#/,
+    },
+    */
+  },
+  tag: {
+    pattern: /(@.+(\n|$))|(\[[^\]]+\])/,
+    greedy: true,
+    inside: {
+      tag: {
+        pattern: /^(\[|@)\/?[^\s(\]|\n)\/]+/,
+        inside: {
+          punctuation: /^\[|@/,
+          namespace: /^[^\s(\]|\n)\/:]+:/,
+        },
+      },
+      /*
+      'special-attr': [
+        {
+          pattern: /\s(exp|cond)=("[^"]*"|[^\s'">=]+(?=[\s\n\]]))/i,
+          lookbehind: true,
+          inside: {
+            'attr-name': /^[^\s=]+/,
+            'attr-value': {
+              pattern: /=[\s\S]+/,
+              inside: {
+                value: {
+                  pattern: /(^=\s*(["']|(?!["'])))\S[\s\S]*(?=\2$)/,
+                  lookbehind: true,
+                  alias: ['javascript', 'language-javascript'],
+                  inside: Prism.languages.javascript,
+                },
+                punctuation: [
+                  {
+                    pattern: /^=/,
+                    alias: 'attr-equals',
+                  },
+                  /"|'/,
+                ],
+              },
+            },
+          },
+        },
+        {
+          pattern: /\s([^=]+)=("&[^"]*"|&[^\s'">=]+(?=[\s\n\]]))/i,
+          lookbehind: true,
+          inside: {
+            'attr-name': /^[^\s=]+/,
+            'attr-value': {
+              pattern: /=[\s\S]+/,
+              inside: {
+                value: {
+                  pattern: /(^=(["']|(?!["']))&)\S[\s\S]*(?=\2$)/,
+                  lookbehind: true,
+                  alias: ['javascript', 'language-javascript'],
+                  inside: Prism.languages.javascript,
+                },
+                punctuation: {
+                  pattern: /^=/,
+                  alias: 'attr-equals',
+                },
+                keyword: {
+                  pattern: /(?<=^["']{0,1})\&/,
+                  alias: 'attr-equals',
+                },
+              },
+            },
+          },
+        },
+      ],
+      */
+      'attr-value': {
+        pattern: /=\s*(?:"[^"]*"|'[^']*'|[^\s'"\]=]+)/,
+        inside: {
+          punctuation: [
+            {
+              pattern: /^=/,
+              alias: 'attr-equals',
+            },
+            {
+              pattern: /^(\s*)["']|["']$/,
+              lookbehind: true,
+            },
+          ],
+          keyword: {
+            pattern: /%|&/,
+            lookbehind: true,
+          },
+        },
+      },
+      punctuation: /\]/,
+      'attr-name': {
+        pattern: /[^\s(\]|\n)\/]+/,
+        inside: {
+          namespace: /^[^\s(\]|\n)\/:]+:/,
+        },
+      },
+    },
+  },
+};
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const pkg = require('./package');
+const date = new Date();
+const header = require('gulp-header');
+
+const header_str = `/* prism-tyranoscript.js v${pkg.version}
+ * (c) ${date.getFullYear()} @ogihara88sai
+ * Released under the MIT Licence */
+`;
+
+function build() {
+  return gulp
+    .src(['./src/prism-tyranoscript.js'])
+    .pipe(header(header_str))
+    .pipe(gulp.dest('./dist'))
+    .pipe(uglify())
+    .pipe(header(header_str))
+    .pipe(
+      rename({
+        extname: '.min.js',
+      })
+    )
+    .pipe(gulp.dest('./dist'));
+}
+
+exports.build = build;
